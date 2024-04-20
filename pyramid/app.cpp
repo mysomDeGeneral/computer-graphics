69318b13e6b321d798b9abf56faac4ccd6513914
Michael Kokonu <fianyekukokonumichael@gmail.com>
2024-04-20 05:00:53 +0000

    removed commented code

--- pyramid/app.cpp
+++ pyramid/app.cpp
@@ -128,23 +128,17 @@ int main(){
     }
     
 
-    // glm::mat4 model = glm::mat4(1.0f);
-    // model = glm::rotate(model, glm::radians(-55.0f),
-    //                             glm::vec3(1.0f, 0.0f, 0.0f));
 
     glm::mat4 view = glm::mat4(1.0f);
-    // note that we’re translating the scene in the reverse direction
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
 
     glm::mat4 projection;
     projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f,100.0f);
-    //glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
 
     glUseProgram(shaderProgram);
 
     int modelLoc = glGetUniformLocation(shaderProgram, "model");
-    //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
     int viewLoc = glGetUniformLocation(shaderProgram, "view");
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
